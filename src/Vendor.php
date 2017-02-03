3b24f02f83e287103b7b2a27bc23f3d461eab9e9
Jose Angel <jangel.romero@gmail.com>
2017-02-03 17:00:07 +0100

    Vendor

--- src/Vendor.php
+++ src/Vendor.php
@@ -12,8 +12,8 @@ use angelrove\utils\CssJsLoad;
 
 class Vendor
 {
-   static private $paths     = array();
-   static private $resources = array();
+   private static $paths     = array();
+   private static $resources = array();
 
    //-------------------------------------------------
    // static function set_path_vendor($path)
@@ -21,15 +21,24 @@ class Vendor
    //    self::$path_vendor = $path;
    // }
    //-------------------------------------------------
-   static function conf($key, $path, $files)
+   public static function conf($key, $path, $files)
    {
       self::$paths[$key]     = $path;
       self::$resources[$key] = $files;
    }
    //-------------------------------------------------
+   public static function usef($key, $op='')
+   {
+      foreach(self::$resources[$key] as $file)
+      {
+         $file_path = self::$paths[$key].$file;
+         CssJsLoad::set($file_path);
+      }
+   }
+   //-------------------------------------------------
    // Composer
    //-------------------------------------------------
-   // static function get_path_vendor($namespace, $classname)
+   // public static function get_path_vendor($namespace, $classname)
    // {
    //    try {
    //       $map = require(PATH_VENDOR.'/composer/autoload_psr4.php');
@@ -46,7 +55,7 @@ class Vendor
    //    }
    // }
    // //-------------------------------------------------
-   // static function get_url_vendor($namespace)
+   // public static function get_url_vendor($namespace)
    // {
    //    // Get Composer autoloads ---
    //    $json = json_decode(file_get_contents(DOCUMENT_ROOT.'/../composer.json'), true);
@@ -59,14 +68,5 @@ class Vendor
    //    return $url;
    // }
    //-------------------------------------------------
-   //-------------------------------------------------
-   static function usef($key, $op='')
-   {
-      foreach(self::$resources[$key] as $file)
-      {
-         $file_path = self::$paths[$key].$file;
-         CssJsLoad::set($file_path);
-      }
-   }
    //-------------------------------------------------
 }
