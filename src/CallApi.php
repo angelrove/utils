85a450893507657eb360213754be2cbe9ee058f2
Jose Angel <jangel.romero@gmail.com>
2018-08-08 11:28:59 +0200

    Crawler: mejoras

--- src/CallApi.php
+++ src/CallApi.php
@@ -94,12 +94,12 @@ class CallApi
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 2);
+        curl_setopt($curl, CURLOPT_TIMEOUT, 4);
+
         // Redirecciones
         curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($curl, CURLOPT_MAXREDIRS, 1);
-
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 2);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 3);
 
         // Curl exec -------
         self::$lastResponse = curl_exec($curl);
