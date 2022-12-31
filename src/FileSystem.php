11d4b0283dfac0c508ef6492f5b5fbaec54187dc
José Angel Romero Vegas <jangel.romero@gmail.com>
2022-12-31 03:09:10 +0100

    Update FileSystem.php

--- src/FileSystem.php
+++ src/FileSystem.php
@@ -39,6 +39,7 @@ class FileSystem
             }
         }
 
+        return $listDeletes;
     }
     //--------------------------------------------------------------------
     /* Renombrar archivos */
